f697f621f8a1d23cb22e1510f08398159958fbc1
Админ <@HOME>
2024-05-27 21:43:20 +0400

    Copy assignment DONE

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-
+#define delimiter "\n---------------------\n"
 //struct Point
 //{
 	//double x;
@@ -56,7 +56,15 @@ public:
 	{
 		cout << "Destructor:\t\t" << this << endl; 
 	}
-	double distance(Point other)
+	//Operators:
+	Point  operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this; 
+	}
+	double distance(const Point& other)
 	{
 		double x_distance = this->x - other.x;
 		double y_distance = this->y - other.y;
@@ -65,10 +73,10 @@ public:
 	}
 	void print()const
 	{
-		cout << "X= " << x << "\tY=" << y << endl;
+		cout <<this<< ":X= " << x << "\tY=" << y << endl;
 	}
 };
-double distance(Point A, Point B)
+double distance(const Point& A, const Point& B)
 {
 	double x_distance = A.get_x() - B.get_x();
 	double y_distance = A.get_y() - B.get_y();
@@ -76,7 +84,8 @@ double distance(Point A, Point B)
 }
 //#define STRUCT_POINT
 //#define DISTANCE_CHECH
-#define CONSTROCTORS_CHECK
+//#define CONSTROCTORS_CHECK
+#define ASSIGMENT_CHECK
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -106,9 +115,13 @@ void main()
 	B.set_y(8);
 	cout << B.get_x() << "\t" << B.get_y() << endl;
 	cout << "Расстояние от точки 'А' до точке 'В':" << A.distance(B) << endl;
+	cout << delimiter << endl; 
 	cout << "Расстояние от точки 'B' до точке 'A':" << B.distance(A) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние между 'A'и 'B':" << distance(A, B) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние между 'B'и 'A':" << distance(B, A) << endl;
+	cout << delimiter << endl;
 #endif // DISTANCE_CHECH 
 
 #ifdef CONSTROCTORS_CHECK
@@ -118,9 +131,26 @@ void main()
 	B.print();
 	Point C(2, 3);
 	C.print();
-	Point D = C;
+	Point D = C;//Copy constructor 
 	D.print();
 #endif // CONSTROCTORS_CHECK
+	
+#ifdef ASSIGMENT_CHECK
+	//Point A(2, 3);//Constructor
+//Point B;//Default constructor 
+//B = A;//Copy assigment 
+//B.print(); 
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << "\t" << b << "\t" << c << endl;
+
+	Point A, B, C;
+	cout << delimiter << endl;
+	A = B = C = Point(2, 3);
+	cout << delimiter << endl;
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGMENT_CHECK
 
 }
-
